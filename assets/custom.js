899f7df87cc86a4aa1845b17a234edd781b243ed
devshopier <remotecoder0@gmail.com>
2024-05-28 16:18:40 -0700

    fix: stock alert for added items

--- assets/custom.js
+++ assets/custom.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
                         // Check status code
                         if (jqXHR.status === 422) {
                             // display error wherever you want to  
-                            $('.not-enough-stock').append(`<p>${jqXHR.responseText}</p>`)
+                            $('.not-enough-stock').append(`<p>${jqXHR.responseText.description}</p>`)
                         } else {
                             $('.not-enough-stock').append(`<p>Not enough stock</p>`)
                         }
